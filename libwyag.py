ea45bf0a4002c6c53ef32330a292f775bb523196
Vedansh Balasaria <vb1024@doc.ic.ac.uk>
2026-09-25 09:45:29 +0100

    Feat: Implement log command and GitCommit

--- libwyag.py
+++ libwyag.py
@@ -70,11 +70,23 @@ What exactly that means depend on each subclass.
 class GitBlob(GitObject):
     fmt=b'blob'
 
-    def serialize(self, repo):
+    def serialize(self):
         return self.blobdata
 
     def deserialize(self, data):
         self.blobdata = data
+
+class GitCommit(GitObject):
+    fmt=b'commit'
+
+    def init(self):
+        self.kvlm = dict()
+
+    def serialize(self):
+        return kvlm_serialize(self.kvlm)
+
+    def deserialize(self, data):
+        self.kvlm = kvlm_parse(data)
 
 '''UTILITY FUNCTIONS'''
 # Asterisk makes the function variadic, which means multiple args can be passed as path. Function receives a list
@@ -248,6 +260,112 @@ def object_hash(fd, fmt, repo=None):
 
     return object_write(obj, repo)
 
+def kvlm_parse(raw, start=0, dct=None):
+    if not dct:
+        dct = dict()
+        # You CANNOT declare the argument as dct=dict() or all call 
+        # to the functions will endlessly grow the same dict.
+
+    # This function is recursive: it reads a key-value pair, then call
+    # itself back with the new position. So we first need to know 
+    # where we are: at a keyword, or already in the message/
+
+    # We search for the next space and the next newline
+    spc = raw.find(b' ', start)
+    nl = raw.find(b'\n', start)
+
+    # If space appears before newline, we have a keyword. Otherwise,
+    # it's the final message, which we just read to the end of the file.
+
+    # Base case
+    # =========
+    # If newline appears first (or there's no space at all, in which
+    # case find returns -1), we assume a blank line. A blank line
+    # means the remainder of the data is the message. We store it in the dictionary,
+    # with None as the key, and return.
+    if (spc < 0) or (nl < spc):
+        assert nl == start
+        dct[None] = raw[start+1:]
+        return dct
+
+    # Recursive case
+    # ==============
+    # We read a key-value pair and recurse for the next.
+    key = raw[start:spc]
+
+    # Find the end of the value. Continuation lines begin with a space, so we loop until we find a "\n" 
+    # not followed by a space. 
+
+    end = start
+    while True:
+        end = raw.find(b'\n', end+1)
+        if raw[end+1] != ord(' '): break
+
+    # Grab the value
+    # Also, drop the leading space on continuation lines
+    value = raw[spc+1: end].replace(b'\n ', b'\n')
+
+    # Don't overwrite the existing data contents
+    if key in dct:
+        if type(dct[key]) == list:
+            dct[key].append(value)
+        else:
+            dct[key] = [ dct[key], value]
+    else:
+        dct[key] = value
+
+    return kvlm_parse(raw, start=end+1, dct=dct)
+
+def kvlm_serialize(kvlm):
+    ret = b''
+
+    # Output fields
+    for k in kvlm.keys():
+        # Skip the message itself
+        if k == None: continue
+        val = kvlm[k]
+        # Normalize to a list
+        if type(val) != list:
+            val = [ val ]
+
+        for v in val:
+            ret += k + b' ' + (v.replace(b'\n', b'\n ')) + b'\n'
+
+        # Append message
+        ret += b'\n' + kvlm[None]
+
+        return ret
+
+def log_graphviz(repo, sha, seen):
+    if sha in seen:
+        return
+    seen.add(sha)
+
+    commit = object_read(repo, sha)
+    message = commit.kvlm[None].decode("utf8").strip()
+    message = message.replace("\\", "\\\\")
+    message = message.replace("\"", "\\\"")
+
+    if "\n" in message: # Keep only the first line
+        message = message[:message.index("\n")]
+
+    print(f"  c_{sha} [label=\"{sha[0:7]}: {message}\"]")
+    assert commit.fmt==b'commit'
+
+    if not b'parent' in commit.kvlm.keys():
+        # Base case: the initial commit
+        return
+
+    parents = commit.kvlm[b'parent']
+
+    if type(parents) != list:
+        parents = [ parents ]
+
+    for p in parents:
+        p = p.decode("ascii")
+        print (f"  c_{sha} -> c_{p};")
+        log_graphviz(repo, p, seen)
+
 '''MAIN - ARG PARSING AND DISPATCHING'''
 argparser = argparse.ArgumentParser(description="A simple Git-like version control system.")
 
@@ -293,6 +411,14 @@ argsp.add_argument("-w",
 argsp.add_argument("path",
                    help="Read object from <file>")
 
+argsp = argsubparsers.add_parser("log", 
+                                 help="Display history of a given commit")
+
+argsp.add_argument("commit",
+                   default="HEAD",
+                   nargs="?",
+                   help="Commit to start at.")
+
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
 
@@ -336,4 +462,11 @@ def cmd_hash_object(args):
     with open(args.path, "rb") as fd:
         sha = object_hash(fd, args.type.encode(), repo)
         print(sha)
-    
+
+def cmd_log(args):
+    repo = repo_find()
+
+    print("digraph wyaglog{")
+    print("  node[shape=rect]")
+    log_graphviz(repo, object_find(repo, args.commit), set())
+    print("}")
